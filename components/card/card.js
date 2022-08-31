9e11dace85361d1514e57eedc78257e9c20dafa0
Tashila <tashila.dev@gmail.com>
2022-08-31 23:05:20 +0400

    Added in error image

--- components/card/card.js
+++ components/card/card.js
@@ -4,7 +4,10 @@ import Image from 'next/image';
 import styles from './card.module.css';
 
 const Card = props => {
-  const { imgUrl = '/static/clifford.webp', size = 'medium' } = props;
+  const {
+    imgUrl = 'https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80',
+    size = 'medium',
+  } = props;
 
   const [imgSrc, setImgSrc] = useState(imgUrl);
 
@@ -16,7 +19,9 @@ const Card = props => {
 
   const handleOnError = () => {
     console.log('hii error');
-    setImgSrc('/static/clifford.webp');
+    setImgSrc(
+      'https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80'
+    );
   };
 
   return (
--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
       <NavBar username='tashila' />
       <Banner title='Clifford the red dog' subTitle='a very cute dog' imgUrl='/static/clifford.webp' />
       <Card imgUrl='/static/clifford.webp' size='large' />
-      <Card imgUrl='/static/clifford.webp' size='medium' />
+      <Card size='medium' />
       <Card imgUrl='/static/clifford.webp' size='small' />
     </div>
   );
