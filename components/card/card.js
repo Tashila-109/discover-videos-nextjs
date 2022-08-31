b2192e4a65874d2f7754f1c313e8f1e48b765eee
Tashila <tashila.dev@gmail.com>
2022-08-31 21:59:54 +0400

    Added in class map

--- components/card/card.js
+++ components/card/card.js
@@ -4,6 +4,12 @@ import styles from './card.module.css';
 
 const Card = props => {
   const { imgUrl, size } = props;
+  const classMap = {
+    large: styles.lgItem,
+    medium: styles.mdItem,
+    small: styles.smItem,
+  };
+
   return (
     <div className={styles.container}>
       Card
