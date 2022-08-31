808d5e63b80593b1021d8b26989e29a74efafb40
Tashila <tashila.dev@gmail.com>
2022-08-31 14:54:13 +0400

    Added in navbar component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 
 import Banner from '../components/banner/banner';
+import NavBar from '../components/nav/navbar';
 
 import styles from '../styles/Home.module.css';
 
@@ -13,6 +14,7 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <h1>Netflix</h1>
+      <NavBar username='tashila' />
       <Banner title='Clifford the red dog' subTitle='a very cute dog' imgUrl='/static/clifford.webp' />
 
       {/* <Navbar />
