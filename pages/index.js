fbf9bc5ad9a5768aba513b7895db1a401253d6b9
Tashila <tashila.dev@gmail.com>
2022-09-01 09:20:17 +0400

    Added in data lib and json object

--- pages/index.js
+++ pages/index.js
@@ -7,18 +7,10 @@ import SectionCards from '../components/card/section-cards';
 
 import styles from '../styles/Home.module.css';
 
+import { getVideos } from '../lib/videos';
+
 export default function Home() {
-  const disneyVideos = [
-    {
-      imgUrl: '/static/clifford.webp',
-    },
-    {
-      imgUrl: '/static/clifford.webp',
-    },
-    {
-      imgUrl: '/static/clifford.webp',
-    },
-  ];
+  const disneyVideos = getVideos();
   return (
     <div className={styles.container}>
       <Head>
