ca9acd652de6fabb8132c4fa6fe4cfddfdf7b839
Tashila <tashila.dev@gmail.com>
2022-09-04 22:57:33 +0400

    Added in Modal styles

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -9,8 +9,7 @@ const Video = () => {
   const router = useRouter();
   console.log({ router });
   return (
-    <div>
-      video page {router.query.videoId}
+    <div className={styles.container}>
       <Modal isOpen={true} contentLabel='Watch the video' onRequestClose={() => router.back()} overlayClassName={styles.overlay}>
         <div>Modal body</div>
       </Modal>
