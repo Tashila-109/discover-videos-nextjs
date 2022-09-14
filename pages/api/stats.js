d180b262ecbcb304a68a2c7b14877d0fb55f629e
Tashila <tashila.dev@gmail.com>
2022-09-14 19:56:17 +0400

    Added in update stats db operation

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import { findVideoIdByUser } from '../../lib/db/hasura';
+import { findVideoIdByUser, insertStats, updateStats } from '../../lib/db/hasura';
 
 export default async function stats(req, resp) {
   if (req.method === 'POST') {
@@ -18,8 +18,15 @@ export default async function stats(req, resp) {
         const doesStatsExist = await findVideoIdByUser(token, userId, videoId);
         if (doesStatsExist) {
           // update it
+          const response = await updateStats(token, {
+            watched: true,
+            userId,
+            videoId: 'gxc6y2ZVfCU',
+          });
+          resp.send({ msg: 'it works', response });
         } else {
           // add it
+          resp.send({ msg: 'it works', decodedToken, doesStatsExist });
         }
         resp.send({ msg: 'it works', decodedToken, doesStatsExist });
       }
