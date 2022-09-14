ab9e4f7dee9bd6bf9dacc56b50b853d8737c5136
Tashila <tashila.dev@gmail.com>
2022-09-14 17:45:30 +0400

    Added in find video id by user db operation

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import { findVideoIdByUser } from '../../lib/db/hasura';
 
 export default async function stats(req, resp) {
   if (req.method === 'POST') {
@@ -9,9 +10,13 @@ export default async function stats(req, resp) {
       if (!token) {
         resp.status(403).send({});
       } else {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        console.log({ decoded });
-        resp.send({ msg: 'it works', decoded });
+        const videoId = req.query.videoId;
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+        const userId = decodedToken.issuer;
+
+        const findVideoId = await findVideoIdByUser(token, userId, videoId);
+        resp.send({ msg: 'it works', decodedToken, findVideoId });
       }
     } catch (error) {
       console.error('Error occurred /stats', error);
