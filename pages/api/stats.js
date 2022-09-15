d75c131d80597b867c650371be3fd00451bb4c03
Tashila <tashila.dev@gmail.com>
2022-09-15 10:41:40 +0400

    Added in GET request for stats api

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -8,13 +8,16 @@ export default async function stats(req, resp) {
       if (!token) {
         resp.status(403).send({});
       } else {
-        const { videoId, favourited, watched = true } = req.body;
+        const { videoId } = req.body;
 
         if (videoId) {
           const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
 
           const userId = decodedToken.issuer;
-          const doesStatsExist = await findVideoIdByUser(token, userId, videoId);
+          const findVideo = await findVideoIdByUser(token, userId, videoId);
+          const doesStatsExist = findVideo?.length > 0;
+
+          const { favourited, watched = true } = req.body;
           if (doesStatsExist) {
             // update it
             const response = await updateStats(token, {
@@ -23,7 +26,7 @@ export default async function stats(req, resp) {
               videoId,
               favourited,
             });
-            resp.send({ msg: 'it works', response });
+            resp.send({ data: response });
           } else {
             // add it
             const response = await insertStats(token, {
@@ -32,13 +35,36 @@ export default async function stats(req, resp) {
               videoId,
               favourited,
             });
-            resp.send({ msg: 'it works', response });
+            resp.send({ data: response });
           }
+        } else {
+          resp.status(500).send({ msg: 'videoId is required' });
         }
       }
     } catch (error) {
       console.error('Error occurred /stats', error);
       resp.status(500).send({ done: false, error: error?.message });
+    }
+  } else {
+    const token = req.cookies.token;
+    if (!token) {
+      resp.status(403).send({});
+    } else {
+      const { videoId } = req.query;
+      if (videoId) {
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+        const userId = decodedToken.issuer;
+        const findVideo = await findVideoIdByUser(token, userId, videoId);
+        const doesStatsExist = findVideo?.length > 0;
+        if (doesStatsExist) {
+          resp.send(findVideo);
+        } else {
+          // add it
+          resp.status(404);
+          resp.send({ user: null, msg: 'Video not found' });
+        }
+      }
     }
   }
 }
