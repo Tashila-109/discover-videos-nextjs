fa9bfeea11c387288955c956ece419c9d1747c01
Tashila <tashila.dev@gmail.com>
2022-09-16 14:59:30 +0400

    Added in my list styling

--- pages/browse/my-list.js
+++ pages/browse/my-list.js
@@ -3,15 +3,17 @@ import NavBar from '../../components/nav/navbar';
 
 import SectionCards from '../../components/card/section-cards';
 
+import styles from "../../styles/MyList.module.css";
+
 const MyList = () => {
   return (
     <div>
       <Head>
         <title>My list</title>
       </Head>
-      <main>
+      <main className={styles.main}>
         <NavBar />
-        <div>
+        <div className={styles.sectionWrapper}>
           <SectionCards title='My List' videos={[]} size='small' />
         </div>
       </main>
